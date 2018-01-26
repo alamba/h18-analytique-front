f129dc7446eff51ba67428f162dc5848014aecce
Simon Cote <cote.simon@live.ca>
2018-01-26 00:30:41 -0500

    Début de l'intégration d'AngularJS, modifications suite aux commentaires des PO

--- js/squidsquads.js
+++ js/squidsquads.js
@@ -1,13 +1,45 @@
-$(document).ready(function () {
+var app = angular.module('squidApp', []);
 
+// Templates
+app.directive('navNotConnected', function () {
+    return {
+        templateUrl: './templates/nav-not-connected-template.html',
+        scope: {
+            activeLink: '@'
+        },
+        link: function () {
+            prepMaterializeCss();
+        }
+    };
+}).directive('navAdminPub', function () {
+    return {
+        templateUrl: './templates/nav-admin-pub-template.html',
+        scope: {
+            activeLink: '@'
+        },
+        link: function () {
+            prepMaterializeCss();
+        }
+    };
+});
+
+
+$(document).ready(function () {
+    prepMaterializeCss();
+});
+
+function prepMaterializeCss() {
+
+    // Dropdown material styling
     $('select').material_select();
 
-    // Activer le side nav
+    // Sidebar on mobile
     $(".button-collapse").sideNav();
 
-    // Activer les dropdowns
+    // Dropdown activation
     $(".dropdown-button").dropdown({belowOrigin: true});
 
+    // Pick a date formatting
     $('.datepicker').pickadate({
         format: 'yyyy/mm/dd',
         monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
@@ -16,4 +48,4 @@ $(document).ready(function () {
         clear: 'Effacer',
         formatSubmit: 'yyyy/mm/dd'
     });
-});
+}
